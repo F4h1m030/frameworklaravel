18fe8c87f83ac5756f4d4b9b3a7d159213fcaee2
F4h1m030 <342094@student.mboutrecht.nl>
2025-09-19 13:12:14 +0200

    vergeten aflevering 3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,6 +20,12 @@
         @endif
     </head>
     <body>
-        <h1>hello world</h1>
-    </body>
+        <nav>
+            <x-nav-link href="/">Home</x-nav-link>
+            <x-nav-link href="about">About</x-nav-link>
+            <x-nav-link href="contact">Contact</x-nav-link>
+        </nav>
+        
+            {{ $slot }}
+</body>
 </html>
